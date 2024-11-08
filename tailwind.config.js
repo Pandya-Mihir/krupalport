d9fcf0d6f9655e03450eb74489205dcd97ebe6c0
Mihir Pandya <156068122+Pandya-Mihir@users.noreply.github.com>
2024-11-09 00:43:25 +0530

    add via upload file
    krupal portfolio added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+        colors: {
+            beige: '#F5F5DC',
+            darkBeige: '#E6E6C6',
+        }
+    }
+},
+  plugins: [],
+}
